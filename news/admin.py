7c3732aa00fb9376c30fd32766add0a21e7ba73d
chaselee0127 <chaselee0127@gmail.com>
2024-07-31 19:02:39 +0900

    Add admin configuration for Story and Comment models

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Story, Comment
 
-# Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'points', 'user', 'created_at')
+    list_filter = ('created_at', 'points')
+    search_fields = ('title', 'url', 'user__username')
+    inlines = [CommentInline]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('story', 'user', 'text', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('text', 'user__username', 'story__title')
